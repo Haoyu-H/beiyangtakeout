5c027844e566e7c89677cf16ba91b9117016a15b
HaoyuHe <2651715358@qq.com>
2024-11-18 20:36:42 +0800

    用户催单部分业务代码

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -637,4 +637,26 @@ public class OrderServiceImpl implements OrderService {
 //            throw new OrderBusinessException("超出配送距离");
 //        }
 //    }
+
+
+    /**
+     * 客户催单
+     * @param id
+     */
+    public void reminder(Long id) {
+        //根据id查询订单
+        Orders ordersDB = orderMapper.getById(id);
+
+        //查询订单是否存在
+        if (ordersDB == null ) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+        Map map = new HashMap();
+        map.put("type",2);
+        map.put("orderId",id);
+        map.put("content","订单号：" + ordersDB.getNumber());
+
+
+        webSocketServer.sendToAllClient(JSON.toJSONString(map));
+    }
 }
